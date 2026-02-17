847b0a2dd36c3778658a67c666f2176689697879
IT-Vision-Pvt-Ltd <ceo@itvision.com.pk>
2026-02-17 13:57:29 +0500

    fix: Set isEmbeddedApp false and remove embedded auth strategy to fix OAuth redirect loop
    Removed unstable new embedded auth strategy and set isEmbeddedApp to false.

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -15,9 +15,7 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
-  future: {
-    unstable_newEmbeddedAuthStrategy: true,
-  },
+    isEmbeddedApp: false,
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
