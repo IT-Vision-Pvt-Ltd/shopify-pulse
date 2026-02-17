e977d580adda38ea963f66c102874e40a694940b
IT-Vision-Pvt-Ltd <ceo@itvision.com.pk>
2026-02-17 13:24:34 +0500

    Remove AppProvider from App component
    Removed AppProvider from the App component.

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,7 +1,6 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
-import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { authenticate } from "../shopify.server";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 
@@ -16,10 +15,7 @@ export default function App() {
   const { apiKey } = useLoaderData<typeof loader>();
 
   return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <Outlet />
-    </AppProvider>
-  );
+      <Outlet  );
 }
 
 // Shopify embedded app best practice: Error boundaries
